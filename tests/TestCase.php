19c339bdf49b72e065396fe0aa6857d94442ab88
darylgilo <kentdarylgilowrk@gmail.com>
2025-10-09 14:59:01 +0800

    test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,27 @@
 
 namespace Tests;
 
+
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    protected $connectionsToTransact = [
+        'mysql',
+        'sqlite',
+        'legacy',
+        'issues',
+        'mysql',
+        'sqlite',
+        'legacy',
+        'issues',
+        'mysql',
+        'sqlite',
+        'legacy',
+        'issues',
+    ];
+    
 }
